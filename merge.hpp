b24b330937f1c06dc5b95e8d3785068a7be114cd
ar00k <arkadiusz.ryczek@onet.pl>
2024-11-28 19:15:30 +0100

    header structure

--- merge.hpp
+++ merge.hpp
@@ -1,6 +1,12 @@
 
 class MergeSort {
-
     public:
+    MergeSort() {};
+    ~MergeSort() {};
+    void sort(int* arr, int size);
+
+    private:
+    void mergeSort(int* arr, int left, int right);
+    void merge(int* arr, int left, int mid, int right);
 
 };
